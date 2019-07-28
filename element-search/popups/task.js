3bf93bd919273fd75f8cdb750286c6476d1efe39
Lesya <olesya.dedlovskaya@gmail.com>
2019-07-28 23:15:21 +0300

    first commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,50 +3,28 @@ modalWindow.classList.add("modal_active");
 
 
 let success =  document.getElementById("modal_success");
-let successButton = document.getElementsByClassName("show-success")[0];
+let successButton = document.querySelector(".show-success");
+let closeButtons = document.querySelectorAll(".modal__close_times");
 
+//переключаю с "Сделать хорошо" на "Хорошо сделано!"
 function changeWindow() {
   modalWindow.classList.remove("modal_active");
   success.classList.add("modal_active");
 }
-
-
-
 successButton.onclick = changeWindow;
 
-let activeWindow = document.getElementsByClassName("modal_active")[0];
+//ищу активное на данный момент окно
 
+// функция убирает класс "modal_active",закрывая окно
 function closeWindow() {
+  let activeWindow = document.querySelector(".modal_active");
   activeWindow.classList.remove("modal_active");
 }
 
-
-let closeButtons = document.getElementsByClassName("modal__close")[0];
-/*let closeMe = Array.from(closeButtons);
-closeMe.onclick = closeWindow;
-
-for (let i = 0; i < closeMe.lenght; i++ ) {
-  let closeButton = closeMe[i];
-  closeButton.onclick = closeWindow;
-}*/
-
-closeButtons.onclick = closeWindow;
-
-/*let elementsForClose = document.getElementsByClassName("modal_active");
-let arrElementsForClose = Array.from(elementsForClose);
-
-let closeButtons = document.getElementsByClassName("modal__close");
+//преобразую HTML-collection 'closeButtons' в массив 
 let closeMe = Array.from(closeButtons);
 
-function closeItNow() {
-  success.classList.remove("modal_active");	
-}
-
-
-for (let i = 0; i < closeMe.lenght; i++ ) {
-  let closeButton = closeMe(i);
-
-  closeButton.onclick = closeItNow;
-}
-*/
-
+//к каждому элементу массива по клику применяю функцию, убирающую класс "modal_active"
+closeMe.forEach(function(i) {
+	i.onclick = closeWindow;
+});
