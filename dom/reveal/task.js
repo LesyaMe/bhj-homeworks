1ab9b6e96bfb37069e234a7b1564fceaa327fe10
Lesya <olesya.dedlovskaya@gmail.com>
2019-08-16 21:37:21 +0300

    цикл не работает

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,53 +1,39 @@
+    
+let advantage = document.querySelectorAll(".reveal");
+let arrAdv = Array.from(advantage);
+let viewportHeight = window.innerHeight;
+
+
+for (let i = 0; i < arrAdv.length; i++) {
+    
+    let elementBottom = arrAdv[i].getBoundingClientRect().bottom;
+    function show () {
+        return arrAdv[i].classList.add("reveal_active");    
+    } 
+
+    if (elementBottom < viewportHeight) {
+        document.addEventListener("scroll", show);    
+    }
+      
+}
+
+
+/*работает только при перезагрузке страницы и открывает блок сразу*/
+
+
+/* ===Вариат, показывающий только 1й блок ===  
 let advantage = document.querySelector(".reveal")
 console.log(advantage.getBoundingClientRect().top);
 
 
 function showElem () {
-	const viewportHeight = window.innerHeight;
+    const viewportHeight = window.innerHeight;
     const elementTop = advantage.getBoundingClientRect().top;
     const elementBottom = advantage.getBoundingClientRect().bottom;
     if (elementTop < viewportHeight) {
-        return advantage.classList.add("reveal_active");	
+        return advantage.classList.add("reveal_active");    
     }
 }
 
 document.addEventListener("scroll", showElem);
-
-
-
-/*
-//вариант 1 нерабтающий
-function showElem (element) {
-	const viewportHeight = window.innerHeight;
-    let elementTop = element.getBoundingClientRect().top;
-    if (elementTop < viewportHeight) {
-        return element.classList.add("reveal_active");	
-    }
-}
-document.onscroll = showElem(advantage);
-
-//window.addEventListener("scroll", showElem(advantage));
-
-*/
-
-
-/*
-// вариант 2 нерабтающий
-
-let B =  function(elem) {
-    const viewportHeight = window.innerHeight;
-    const elementTop = elem.getBoundingClientRect().top;
-    if (elementTop < viewportHeight) {
-        return elem.classList.add("reveal_active");	
-    }
-};
-
-window.onscroll = B(advantage);
-*/
-
-
-
-
-/* В двух закоментированных вариантах я пытаюсь передать в функцию аргумент извне,
-чтобы предусмотерть возможность нескольких таких элементов на странице, но ничего не выходит.
-Подскажите, пожалуйста, в чем моя ошибка?*/ 
+======================================================*/
