e9dd042660253b3d528b6e23626c30879e27ffa6
Lesya <olesya.dedlovskaya@gmail.com>
2019-08-16 22:15:40 +0300

    исправлено написание current, Я все еще отображается

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,8 +2,7 @@ let rotattor = document.querySelectorAll(".rotator__case");
 let arrRotattor = Array.from(rotattor);
 
  
-/*Уместно ли писать так индекс?: arrRotattor[current++].classList.add("rotator__case_active");
-Почему в переключении отражается просто "Я"? т.е. у всех элементов отсутствует активный класс?
+/*Почему в переключении отражается просто "Я"? т.е. у всех элементов отсутствует активный класс?
 Rак это иправить?*/
 
 let current = 1;
@@ -14,13 +13,28 @@ function showNextCase(n) {
 	if (n < 1) {
 		current = arrRotattor.length;
 	} else if (n > arrRotattor.length) {
+		n = 0;
 		current = 1;
 	}
 
 	for (let i = 0; i < arrRotattor.length; i++ ) {
 		arrRotattor[i].classList.remove("rotator__case_active");
 	}
-    arrRotattor[current++].classList.add("rotator__case_active");
+	current = current+=1;
+    arrRotattor[current].classList.add("rotator__case_active");
 }
 
+//  в этом виде все работет, только одним из элементов цикла появляется "Я"
 let id = setInterval("showNextCase(current)", 1000);
+
+/*========так ничего не работает==============
+let id = setInterval(showNextCase(current), 700);
+============================================*/
+
+
+/*============так проходит только 1 цикл, завершается на "Я"
+let id = setInterval('showNextCase()', 700);
+===========================================*/
+
+
+
